296937f6b7486987527c969bb7d41edd437e7db9
Zachary <ztownes@mysticturtles.com>
2019-03-26 22:32:24 -0600

    Add project files.

---
+++ BudgetApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+
+
+namespace BudgetApp
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API Configuration and Services
+
+            //Web API routs
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional}
+            );
+
+
+        }
+    }
+}
---
+++ BudgetApp/Controllers/DateObjects.cs
@@ -0,0 +1,22 @@
+﻿using Budget;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BudgetApp.Controllers
+{
+    public class DateObjectController : ApiController
+    {
+        budgetEntities _db = new budgetEntities();
+
+        public IHttpActionResult Get()
+        {
+            return Ok(new { Name = "Zachary", Occupation = "Help Desk Analyst" });
+        }
+    }
+}
