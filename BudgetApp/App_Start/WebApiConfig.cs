bcbc3e7a728e38f1daaf0a8b6b396943c43b94c9
Zachary Townes <zachtownes@gmail.com>
2019-03-28 20:39:48 -0600

    Updated Route, Added Get Month and Get and Add year to DateObject API Modified budget model to accept years properly

--- BudgetApp/App_Start/WebApiConfig.cs
+++ BudgetApp/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,13 +16,16 @@ namespace BudgetApp
         {
             // Web API Configuration and Services
 
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
+
             //Web API routs
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional}
+                defaults: new { id = RouteParameter.Optional }
             );
 
 
--- BudgetApp/Controllers/DateObjects.cs
+++ BudgetApp/Controllers/DateObjects.cs
@@ -9,16 +9,125 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 
+
+
 namespace BudgetApp.Controllers
 {
+
     public class DateObjectController : ApiController
     {
         budgetEntities _db = new budgetEntities();
 
-        public IHttpActionResult Get()
-        {
+        
 
-            return Ok(new { Name = "Zachary", Occupation = "Help Desk Analyst" });
+        public IHttpActionResult GetMonths([FromUri] bool GetMonth)
+        {
+            var dateObjects = (from mon in _db.months
+                               where mon.active == 1
+                               select new Months
+                               {
+                                   monthName = mon.monthName,
+                                   monthInt = mon.month_int
+                               }).ToList();
+
+            if (dateObjects.FirstOrDefault() != null)
+            {
+                return Ok(dateObjects);
+            }
+            else
+            {
+                return StatusCode(HttpStatusCode.NoContent);
+            }
         }
+
+        public IHttpActionResult GetYears([FromUri] bool GetYear)
+        {
+            var dateObjects = (from y in _db.years
+                               where y.active == 1
+                               select new Years
+                               {
+                                   yearInt = y.yearInt
+                               }).ToList();
+
+            if (dateObjects.FirstOrDefault() != null)
+            {
+                return Ok(dateObjects);
+            }
+            else
+            {
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult AddYear([FromUri] bool AddYear, [FromUri] int year)
+        {
+            var Years = (from y in _db.years
+                         where y.yearInt == year
+                         select new YearData
+                         {
+                            yearInt = y.yearInt
+
+                         }).ToList ();
+            if (Years.FirstOrDefault() != null)
+            {
+                if (Years.FirstOrDefault().active == 1)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    _db.years.Where(i => i.yearInt == year).FirstOrDefault().active = 1;
+                    _db.SaveChanges();
+                }
+            }
+            else
+            {
+                var yeartoAdd = new year();
+                yeartoAdd.active = 1;
+                yeartoAdd.yearInt = year;
+                _db.years.Add(yeartoAdd);
+                _db.SaveChanges();
+            }
+            var YearsAdded = (from y in _db.years
+                         where y.yearInt == year
+                         select new YearData
+                         {
+                             yearInt = y.yearInt
+                         }).ToList();
+            if(YearsAdded.FirstOrDefault() != null)
+            {
+                return Created("YearTable", year);
+
+            }
+            else
+            {
+                return InternalServerError();
+            }
+        }
+    }
+
+    public class MonthData
+    {
+        public string monthName { get; set; }
+        public int monthInt { get; set; }
+        public int active { get; set; }
+    }
+
+    public class YearData
+    {
+        public int yearInt { get; set; }
+        public int active { get; set; }
+    }
+
+    public class Years
+    {
+        public int yearInt { get; set; }
+    }
+
+    public class Months
+    {
+        public string monthName { get; set; }
+        public int monthInt { get; set; }
     }
 }
