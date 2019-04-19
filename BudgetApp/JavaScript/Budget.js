397a03decbffb5158c8b6c13c1287fe44b3b798c
Zachary Townes <zachtownes@gmail.com>
2019-04-19 12:11:33 -0600

    Created Current Balance Section to budgetmain.html, Added Loading-Bar.css and Loading-Bar.JS library, Created the Current Balance JS logic,

--- BudgetApp/JavaScript/Budget.js
+++ BudgetApp/JavaScript/Budget.js
@@ -7,8 +7,16 @@ navBarToggle.addEventListener('click', function () {
 })
 
 function onLoad() {
+    var today = new Date()
     $.ajax({
         method: "GET",
-        url: "..api/Transactions?getBalance=true&year="+currYear
+        url: "../api/Transactions?getBalance=true&year=" + today.getFullYear(),
+        dataType: "json",
+        success: function (data) {
+            if (data > 0) {
+                document.getElementById('BalanceDiv').classList.add('panel-success')
+                document.getElementById('BalanceNumber').innerHTML = 'Current Projected Balance : $' + data;
+            }
+        }
     });
 }
